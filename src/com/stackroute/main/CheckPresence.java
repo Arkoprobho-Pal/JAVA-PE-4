3fec085df4896e82ec164a21497ba7b3c6b99979
Arkoprobho-Pal <arkoprobho.helloworld@gmail.com>
2019-09-25 13:37:22 +0530

    CheckPresence change in implementation

--- src/com/stackroute/main/CheckPresence.java
+++ src/com/stackroute/main/CheckPresence.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class CheckPresence {
     public void PresenceString(){
-        String target="Harry";
+
         Scanner input=new Scanner(System.in);
         System.out.println("Input Query string :");
         String queryString=input.nextLine();
-        System.out.print("Is Harry here ?");
-        if(queryString.indexOf(" "+target)!=-1){
+        System.out.print("Is Harry here ? "+queryString.matches("(?i).*Harry.*"));
+       /* if(queryString.indexOf(" "+target)!=-1){
             System.out.print(" true");
         }
         else
         {
             System.out.println(" False");
-        }
+        }*/
     }
 
     public static void main(String[] args) {
